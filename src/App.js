6ac63e2512c24e92248fe3d1306b71fcecdb3d8c
Yani666 <yanis051989@gmail.com>
2021-06-24 14:27:22 -0500

    half

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
+
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Routes from './Routes'
+import CountriesList from './components/CountriesList';
+import DataCountries from './countries.json'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+class App extends Component{
+
+  state={
+    listC:DataCountries
+  }
+  render(){
+
+    const {listC} = this.state
+    console.log("que es estoo", listC)
+
+    return(
+    <div className ="App">
+
+      <Navbar/>
+      <div className="row-app">
+      <CountriesList countries={listC}/>
+      <Routes/>
+      </div>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
